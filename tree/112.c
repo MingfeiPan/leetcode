988b4ae7b93ec759ce465779597355b41a368575
Mingfei <113104667@umail.ucc.ie>
2017-12-07 19:51:48 +0800

    112

---
+++ tree/112.c
@@ -0,0 +1,14 @@
+/**
+ * 符合要求的叶子结点特征: left==null right==null val==sum
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum(struct TreeNode* root, int sum) {
+    if(root==NULL) return false;
+    if(root->left==NULL && root->right==NULL && root->val == sum) return true;
+    return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+}
