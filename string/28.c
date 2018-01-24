36a66883a13c30a6f837ceed1842b5931bf3c22e
Mingfei <113104667@umail.ucc.ie>
2018-01-24 17:56:06 +0800

    28

--- string/28.c
+++ string/28.c
@@ -18,3 +18,50 @@ int strStr(char* haystack, char* needle) {
     }
     return -1;
 }
+
+
+//cmp in O(n)
+int* buildNext(char* tar)
+{
+	int m = strlen(tar);
+	int j = 0;
+	int* next = (int*)malloc(sizeof(int)*m);
+	int t = next[0] = -1;
+	while(j < m - 1)
+	{
+		if( t < 0 || tar[j] == tar[t])
+		{
+			next[++j] = ++t;
+		}
+		else
+		{
+			t = next[t];
+		}
+	}
+	return next;
+}
+
+int strStr(char* haystack, char* needle)
+{
+	int* next = buildNext(needle);
+	int n = strlen(haystack);
+	int i = 0;
+	int m = strlen(needle);
+	int j = 0;
+	while(j < m && i < n)
+	{
+		if(j < 0 || needle[j] == haystack[i])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			j = next[j];
+		}
+	}
+	free(next);
+    int ret = i - j;
+    if(n < ret + m) return -1;
+    else return ret;
+}
