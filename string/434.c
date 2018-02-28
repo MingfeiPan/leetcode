297258d1d1c42f684840d5a21c3a12f59cd6a962
Mingfei <113104667@umail.ucc.ie>
2018-02-28 23:58:31 +0800

    434

--- string/434.c
+++ string/434.c
@@ -1,8 +1,9 @@
-class Solution:
-    def countSegments(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        return len(s.split())
-        
+int countSegments(char* s) {
+    int count = 0;
+    while(*s != '\0'){
+        if(*s != ' ' && (*(s+1) == ' ' || *(s+1) == '\0'))
+            count++;
+        s++;
+    }
+    return count;
+}
