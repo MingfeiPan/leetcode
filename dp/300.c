0d4a86c51e165e26534cc3849731976f78971309
Mingfei <113104667@umail.ucc.ie>
2018-03-29 10:44:14 +0800

    up

--- dp/300.c
+++ dp/300.c
@@ -5,14 +5,11 @@ int lengthOfLIS(int* nums, int numsSize) {
     int i;
     for(i = 0; i < numsSize; i++)
         arr[i] = 1;
-    int max;
     for(i = 1; i < numsSize; i++){
-        max = 1;
         for(int j = 0; j < i; j++){
             if(nums[j] < nums[i])
-                max = max < arr[j] + 1 ? arr[j] + 1 : max;
+                arr[i] = arr[i] < arr[j] + 1 ? arr[j] + 1 : arr[i];
         }
-        arr[i] = max;
     }
     int ret = 1;
     for(i = 0; i < numsSize; i++){
