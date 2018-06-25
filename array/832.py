d0ab837bbc33ed84b743680d48f2e647439948aa
Mingfei <113104667@umail.ucc.ie>
2018-06-25 11:53:59 +0800

    832

--- array/832.py
+++ array/832.py
@@ -4,5 +4,8 @@ class Solution:
         :type A: List[List[int]]
         :rtype: List[List[int]]
         """
-        
-        return [[i^1 for i in arr[::-1]] for arr in A]
+        for arr in A:
+            arr.reverse()
+            arr[::] = map(lambda a : a ^ 1, arr)
+        return A
+        # return [[i^1 for i in arr[::-1]] for arr in A]
