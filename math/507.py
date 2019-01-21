f33fb7e30e98e7d61172001bb5660dd34fc9b602
Mingfei <113104667@umail.ucc.ie>
2019-01-21 19:44:32 +0800

    Perfect Number

--- math/507.py
+++ math/507.py
@@ -13,3 +13,23 @@ class Solution:
             if num % i == 0:
                 divisors.append(i)
         return sum(divisors) == num
+
+
+#better
+class Solution:
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        ret = []
+        for i in range(1, int(num ** 0.5) + 1):
+            if num % i == 0 and i != num:
+                ret.append(i)
+                if num / i != i and num / i != num:
+                    ret.append(num // i)
+        
+        return sum(ret) == num
+        #return num == sum((i for i in range(1,  num // 2 + 1) if num % i == 0))
