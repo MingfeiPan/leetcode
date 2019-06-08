e5c1e2564e74a34f67b50f5a00c674df14e029c2
Mingfei <113104667@umail.ucc.ie>
2019-06-08 18:18:15 +0800

    up

--- tree/113.py
+++ tree/113.py
@@ -22,3 +22,35 @@ class Solution:
             self.dfs(root.left, left - root.val, cur[:] + [root.val], ret) 
             self.dfs(root.right, left - root.val, cur[:] + [root.val], ret) 
     
+
+
+# non-recursive
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        if not root:
+            return []
+        ret = []
+        l = []
+        l.append((root, [], sum))
+        
+        while l:
+            cur, cur_list, left = l.pop()
+            if not cur.left and not cur.right:
+                if cur.val == left:
+                    ret.append(cur_list[:] + [cur.val])
+            if cur.left:
+                l.append((cur.left, cur_list[:] + [cur.val], left - cur.val))
+            if cur.right:
+                l.append((cur.right, cur_list[:] + [cur.val], left - cur.val))
+                
+        return ret
+        
