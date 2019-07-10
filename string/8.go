81ee31f5b85b818de4a34f0a143aed7b811c5aec
Mingfei <113104667@umail.ucc.ie>
2019-07-10 14:32:47 +0800

    String to Integer (atoi)

---
+++ string/8.go
@@ -0,0 +1,33 @@
+func myAtoi(str string) int {
+    var (
+        signFlag bool
+        ret = 0
+        index int
+        length = len(str)
+    )
+    
+    for index < length && str[index] == ' ' {
+        index++
+    }
+    
+    if index < length && str[index] == '-' {
+        signFlag = true
+        index++
+    }else if index < length && str[index] == '+' {
+        index++
+    }
+    for index < length && byte(str[index]) <= 57 && byte(str[index]) >= 48 && ret < math.MaxInt32{
+        ret *= 10
+        ret += int(str[index]) - 48
+        index++
+    }
+    if signFlag {
+        ret = -ret
+    }
+    if ret > math.MaxInt32 {
+        return math.MaxInt32
+    }else if ret < math.MinInt32 {
+        return math.MinInt32
+    }
+    return ret
+}
