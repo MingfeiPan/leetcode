60bde5cc3b5296d2a6a46e792a2f00f2363d0d8d
Mingfei <113104667@umail.ucc.ie>
2019-07-20 16:55:03 +0800

    up

--- array/81.go
+++ array/81.go
@@ -35,3 +35,46 @@ func search(nums []int, target int) bool {
     }
     return false
 }
+
+func search(nums []int, target int) bool {
+    if len(nums) == 0 {
+        return false
+    }
+    
+    var high, low, mid, realmid, pivot int
+    high = len(nums) - 1
+    //find the pivot, pivot will be the smallest value as well
+    for low < high {
+        
+        //incase of dup
+        for low < high && nums[low] == nums[high] {
+            low++
+        }
+        mid = (low + high) / 2
+        if nums[mid] > nums[high] {
+            //异常区段
+            low = mid + 1
+        }else {
+            high = mid
+        }
+    }
+    
+    pivot = low //find pivot when low == high, where nums[low-1] > nums[high]
+    low, high = 0, len(nums) - 1
+    
+    //normal binary search with pivot high 
+    for low <= high {
+        mid = (low + high) / 2
+        realmid = (mid + pivot) % len(nums)
+        if nums[realmid] == target {
+            return true
+        }
+        if nums[realmid] < target {
+            low = mid + 1
+        }else {
+            high = mid - 1
+        }
+    }
+    return false
+    
+}
