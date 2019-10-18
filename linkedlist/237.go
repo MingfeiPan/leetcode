211e5a383e1d45250f0d865d5af85c3dbdc4098b
Mingfei <113104667@umail.ucc.ie>
2019-10-19 01:00:00 +0800

    Delete Node in a Linked List

---
+++ linkedlist/237.go
@@ -0,0 +1,12 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+    // swap with next
+    node.Val = node.Next.Val
+    node.Next = node.Next.Next
+}
