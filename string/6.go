f31e20a29e12ccdb9bf3d2617ef10d17a40dcfff
Mingfei <113104667@umail.ucc.ie>
2019-11-24 19:22:20 +0800

    up

--- string/6.go
+++ string/6.go
@@ -5,6 +5,7 @@ func convert(s string, numRows int) string {
     ret := make([]string, numRows)
     var rowindex int
     var direct bool
+	// 每一列都分一个子string, 按方向来回遍历, 每次遇到rowindex为0或者为numRows-1的时候反转
     for _, c := range s {
         ret[rowindex] += string(c)
         if rowindex == 0 || rowindex == numRows - 1 {
