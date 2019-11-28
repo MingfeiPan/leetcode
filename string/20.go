0c657d2c5518e6abade8ceeca37d75e12e03cf09
Mingfei <113104667@umail.ucc.ie>
2019-11-28 23:56:36 +0800

    Valid Parentheses

---
+++ string/20.go
@@ -0,0 +1,31 @@
+func isValid(s string) bool {
+    stack := make([]byte, 0)
+    for i := 0; i < len(s); i++ {
+        switch s[i] {
+            case '(', '{', '[':
+            stack = append(stack, s[i])
+            case ')':
+            if len(stack) == 0 || stack[len(stack)-1] != '(' {
+                return false
+            }else {
+                stack = stack[:len(stack)-1]
+            }
+            case '}':
+            if len(stack) == 0 || stack[len(stack)-1] != '{' {
+                return false
+            }else {
+                stack = stack[:len(stack)-1]
+            }            
+            case ']':
+            if len(stack) == 0 || stack[len(stack)-1] != '[' {
+                return false
+            }else {
+                stack = stack[:len(stack)-1]
+            }            
+        }
+    }
+    if len(stack) > 0 {
+        return false
+    }
+    return true
+}
