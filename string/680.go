ef530aad22534422f6208f3d0698bdf575821746
Mingfei <113104667@umail.ucc.ie>
2019-12-08 10:22:37 +0800

    up

--- string/680.go
+++ string/680.go
@@ -19,3 +19,12 @@ func directCheck(s string) bool {
     }
     return false
 }
+
+func directCheck(s string) bool {
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+        if s[i] != s[j] {
+            return false
+        }        
+    }
+    return true   
+}
