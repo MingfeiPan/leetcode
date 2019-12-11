4d3ec23d6e9f3245a699dd005bb86fb9218ba345
Mingfei <113104667@umail.ucc.ie>
2019-12-11 21:41:49 +0800

    up

--- string/5.go
+++ string/5.go
@@ -30,3 +30,32 @@ func longestPalindrome(s string) string {
     }
     return ret
 }
+
+func longestPalindrome(s string) string {
+    if s == "" {
+        return s
+    }
+    dp := make([][]int, len(s))
+    for i, _ := range dp {
+        dp[i] = make([]int, len(s))
+    }
+    var start, end int
+    for i := len(s)-1; i >= 0; i-- {
+        for j := i; j < len(s); j++ {
+            if s[i] == s[j] {
+                if j - i < 3 {
+                    dp[i][j] = j - i + 1
+                }else {
+                    if dp[i+1][j-1] > 0 {
+                        dp[i][j] = dp[i+1][j-1] + 2    
+                    }
+                }
+                if dp[i][j] > end - start {
+                    start = i
+                    end = j
+                }
+            }
+        }
+    }
+    return s[start:end+1]
+}
