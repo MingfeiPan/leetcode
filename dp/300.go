c94a3ed584fb9291651c12a4afda62cbd2f11850
Mingfei <113104667@umail.ucc.ie>
2019-12-21 18:53:21 +0800

    up

--- dp/300.go
+++ dp/300.go
@@ -20,3 +20,27 @@ func lengthOfLIS(nums []int) int {
     }
     return ret[0]
 }
+
+func lengthOfLIS(nums []int) int {
+    if len(nums) == 0 {
+        return 0
+    }
+    
+    dp := make([]int, len(nums))
+    dp[0] = 1
+    ret := 1
+    for i := 1; i < len(nums); i++ {
+        dp[i] = 1 
+        for j := i-1; j >= 0; j-- {
+            if nums[i] > nums[j] {
+                if dp[i] < dp[j] + 1 {
+                    dp[i] = dp[j] + 1
+                }
+            }
+        }
+        if ret < dp[i] {
+            ret = dp[i]
+        }
+    }
+    return ret
+}
