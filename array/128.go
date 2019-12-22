e28e38d105644a6b3c1fad72729752bfa8489eab
Mingfei <113104667@umail.ucc.ie>
2019-12-22 15:44:51 +0800

    Longest Consecutive Sequence no sort

--- array/128.go
+++ array/128.go
@@ -22,3 +22,32 @@ func longestConsecutive(nums []int) int {
     return ret
     
 }
+
+func longestConsecutive(nums []int) int {
+    m := make(map[int]struct{})
+    for _, num := range nums {
+        if _, ok := m[num]; !ok {
+            m[num] = struct{}{}
+        }
+    }
+    var ret int
+    for _, num := range nums {
+        // 检查当前num 是否已经在一个答案序列里了, 这一步非常tricky
+        if _, ok := m[num-1]; !ok {
+            temp := num
+            count := 1
+            for {
+                if _, ok := m[temp+1]; ok {
+                    temp++
+                    count++
+                }else {
+                    break
+                }
+            }
+            if ret < count {
+                ret = count
+            }            
+        }
+    }
+    return ret
+}
