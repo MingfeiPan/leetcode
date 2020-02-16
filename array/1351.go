f4cee2e3d9ded14b2a831f4d062fc118dad35098
Mingfei <113104667@umail.ucc.ie>
2020-02-16 19:42:22 +0800

    Count Negative Numbers in a Sorted Matrix

--- array/1351.go
+++ array/1351.go
@@ -16,3 +16,30 @@ func countNegatives(grid [][]int) int {
     }
     return ret
 }
+
+func countNegatives(grid [][]int) int {
+    if len(grid) == 0 {
+        return 0
+    }
+    var ret int
+    volume := len(grid[0])
+    for index, _ := range grid {
+        start := 0
+        end := volume
+        var mid int
+        for start < end {
+            mid = start + (end - start) / 2
+            if grid[index][mid] < 0 {
+                if mid == 0 || grid[index][mid-1] >= 0{
+                    ret += volume - mid
+                    break
+                }else {
+                    end = mid
+                }
+            }else {
+                start = mid+1
+            }
+        }
+    }
+    return ret
+}
