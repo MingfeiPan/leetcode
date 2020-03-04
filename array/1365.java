28368539261ec7de6ba7f24fa3281a9f08f7ef17
Mingfei <113104667@umail.ucc.ie>
2020-03-04 22:29:32 +0800

    How Many Numbers Are Smaller Than the Current Number o(n)

--- array/1365.java
+++ array/1365.java
@@ -12,3 +12,26 @@ class Solution {
         return ret;
     }
 }
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ret = new int[nums.length];
+        int[] count = new int[101];
+        for (int i : nums) {
+            count[i]++;
+        }
+        // 对count做累加
+        for (int i = 1; i < count.length; i++) {
+            count[i] += count[i-1];
+        }
+        
+        for (int i = 0; i < ret.length; i++) {
+            if (nums[i] == 0) {
+                ret[i] = 0;
+            }else {
+                ret[i] = count[nums[i]-1];    
+            }
+        }
+        return ret;
+    }
+}
