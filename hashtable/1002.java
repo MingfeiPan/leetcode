24233f1d6dbead2be6c6cb22758cdaa994f97697
Mingfei <113104667@umail.ucc.ie>
2020-04-15 23:35:13 +0800

    up

--- hashtable/1002.java
+++ hashtable/1002.java
@@ -8,7 +8,7 @@ class Solution {
             for (int i = 0; i < s.length(); i++) {
                 String c = String.valueOf(s.charAt(i));
                 if (map.containsKey(c)) {
-                    map.put(c, map.get(c)+1);
+                    map.put(c, map.get(c) + 1);
                 } else {
                     map.put(c, 1);
                 }
