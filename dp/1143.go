b9ecf18cb914e2a2138a57f967bdb1115c0d7c40
Mingfei <113104667@umail.ucc.ie>
2020-04-30 23:33:29 +0800

    Longest Common Subsequence

--- dp/1143.go
+++ dp/1143.go
@@ -22,3 +22,40 @@ func longestCommonSubsequence(text1 string, text2 string) int {
     }
     return ret[length1][length2]
 }
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+    dp := make([][]int, len(text1)+1)
+    for i,_ := range dp {
+        dp[i] = make([]int, len(text2)+1)
+    }
+    for i := 0; i <= len(text1); i++ {
+        for j := 0; j <= len(text2); j++ {
+            if i == 0 || j == 0 {
+                dp[i][j] = 0;
+            } else {
+                if (text1[i-1] == text2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+                }
+            }
+        }
+    }
+    return dp[len(text1)][len(text2)]
+}
+
+func max(i1 int, i2 int, i3 int) int {
+    if i1 < i2 {
+        if i2 < i3 {
+            return i3
+        }else {
+            return i2
+        }
+    }else {
+        if i1 < i3 {
+            return i3
+        }else {
+            return i1;
+        }
+    }
+}
