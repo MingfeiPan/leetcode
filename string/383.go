1aace06f24fc2eb6276d991b48a36801f234490f
Mingfei <113104667@umail.ucc.ie>
2020-05-03 22:50:32 +0800

    Ransom Note

--- string/383.go
+++ string/383.go
@@ -1,24 +1,29 @@
 func canConstruct(ransomNote string, magazine string) bool {
-    m := make(map[rune]int)
+    ransomMap := make(map[rune]int)
+    magazineMap := make(map[rune]int)
     for _, c := range ransomNote {
-        if _, ok := m[c]; !ok {
-            m[c] = 1
-        }else {
-            m[c]++
+        if _, ok := ransomMap[c]; ok {
+            ransomMap[c] += 1
+        } else {
+            ransomMap[c] = 1
         }
     }
+
     for _, c := range magazine {
-        if i, ok := m[c]; ok {
-            if i == 0 {
-                continue
-            }else {
-                m[c]--
-            }
+        if _, ok := magazineMap[c]; ok {
+            magazineMap[c] += 1
+        } else {
+            magazineMap[c] = 1
         }
-    }
-    for _, c := range m {
-        if c > 0 {
+    }    
+    
+    for k, v := range ransomMap {
+        if count, ok := magazineMap[k]; !ok {
             return false
+        } else {
+            if count < v {
+                return false
+            }
         }
     }
     return true
