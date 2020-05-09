3d04b9649b01a3b76c052ad805cc58572ebdff8d
Mingfei <113104667@umail.ucc.ie>
2020-05-09 23:22:40 +0800

    Cousins in Binary Tree

--- tree/993.go
+++ tree/993.go
@@ -38,3 +38,39 @@ func findNode(root *TreeNode, parent *TreeNode, depth int, val int) (retp *TreeN
     }
     return retp, retd
 }
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isCousins(root *TreeNode, x int, y int) bool {
+    var depthx, depthy int
+    var prex, prey *TreeNode
+    dfs(root, nil, 0, x, y, &depthx, &depthy, &prex, &prey)
+    if depthx == depthy && prex != prey {
+        return true
+    }
+    return false
+}
+
+func dfs(root *TreeNode, pre *TreeNode, depth int, x int, y int, depthx *int, depthy *int, prex **TreeNode, prey **TreeNode) {
+    if root == nil {
+        return
+    }
+    if root.Val == x {
+        *depthx = depth
+        *prex = pre
+        return
+    }else if root.Val == y {
+        *depthy = depth
+        *prey = pre
+        return
+    }else {
+        dfs(root.Left, root, depth+1, x, y, depthx, depthy, prex, prey)
+        dfs(root.Right, root, depth+1, x, y, depthx, depthy, prex, prey)
+    }
+}
