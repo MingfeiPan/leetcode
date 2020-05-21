7810a7070a2ef5b40965e851af275e313ecf056c
Mingfei <113104667@umail.ucc.ie>
2020-05-21 23:43:29 +0800

    Odd Even Linked List

--- linkedlist/328.go
+++ linkedlist/328.go
@@ -29,3 +29,28 @@ func oddEvenList(head *ListNode) *ListNode {
     return dummyOdd
     
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func oddEvenList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil || head.Next.Next == nil {
+        return head
+    }
+    
+    dummy := &ListNode{-1, head}
+    dummy2 := &ListNode{-1, head.Next}
+    for head != nil && head.Next != nil && head.Next.Next != nil {
+        cur := head.Next
+        next := head.Next.Next
+        head.Next = next
+        cur.Next = next.Next
+        head = head.Next
+    }
+    head.Next = dummy2.Next
+    return dummy.Next
+}
