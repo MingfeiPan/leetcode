5339d4d663fcfcfbcadef68b3736266927fc8e32
Mingfei <113104667@umail.ucc.ie>
2020-06-02 23:52:13 +0800

    Delete Node in a Linked List

--- linkedlist/237.go
+++ linkedlist/237.go
@@ -10,3 +10,23 @@ func deleteNode(node *ListNode) {
     node.Val = node.Next.Val
     node.Next = node.Next.Next
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+    for node != nil && node.Next != nil {
+        if node.Next.Next == nil {
+            node.Val = node.Next.Val
+            node.Next = nil
+            break
+        }
+        node.Val = node.Next.Val
+        node = node.Next
+        
+    }
+}
