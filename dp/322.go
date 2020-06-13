4c1ddf30574df004d9be0c870c09b1d9463d24cb
Mingfei <113104667@umail.ucc.ie>
2020-06-13 23:46:44 +0800

    Coin Change

--- dp/322.go
+++ dp/322.go
@@ -25,3 +25,30 @@ func min(n1, n2 int) int {
 		return n2
 	}
 }
+
+func coinChange(coins []int, amount int) int {
+    dp := make([]int, amount+1)
+    dp[0] = 0
+    for i := 1; i <= amount; i++ {
+        dp[i] = amount+1
+    }
+    for i := 1; i <= amount; i++ {
+        for _, coin := range coins {
+            if i - coin >= 0 {
+                dp[i] = min(dp[i], dp[i-coin]+1)
+            }
+        }
+    }
+    if dp[amount] > amount {
+        return -1
+    }
+    return dp[amount]
+}
+
+func min(i int, j int) int {
+    if i < j {
+        return i
+    }else {
+        return j
+    }
+}
