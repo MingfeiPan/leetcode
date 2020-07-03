315cd0d288371c6c062bfe8b766370ade0698d44
Mingfei <113104667@umail.ucc.ie>
2020-07-03 23:33:12 +0800

    Binary Tree Level Order Traversal II

--- tree/107.go
+++ tree/107.go
@@ -39,3 +39,42 @@ func levelOrderBottom(root *TreeNode) [][]int {
     return ret
 }
 
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type Item struct {
+    Node *TreeNode
+    Level int
+}
+func levelOrderBottom(root *TreeNode) [][]int {
+    ret := make([][]int, 0)
+    if root == nil {
+        return ret
+    }
+    stack := make([]Item, 0)
+    stack = append(stack, Item{root, 0})
+    for len(stack) > 0 {
+        cur := stack[0]
+        stack = stack[1:]
+        if len(ret) <= cur.Level {
+            ret = append(ret, make([]int, 0))
+        }
+        ret[cur.Level] = append(ret[cur.Level], cur.Node.Val)
+        if cur.Node.Left != nil {
+            stack = append(stack, Item{cur.Node.Left, cur.Level+1})
+        }
+        if cur.Node.Right != nil {
+            stack = append(stack, Item{cur.Node.Right, cur.Level+1})
+        }
+    }
+    for i, j := 0, len(ret)-1; i < j; i, j = i+1, j-1 {
+        ret[i], ret[j] = ret[j], ret[i]
+    }
+    return ret
+}
