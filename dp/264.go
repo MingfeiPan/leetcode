eb8754b808057a69ada6fae32b3da2b13a9f30b7
Mingfei <113104667@umail.ucc.ie>
2020-07-06 23:25:16 +0800

    Ugly Number II

--- dp/264.go
+++ dp/264.go
@@ -29,3 +29,41 @@ func min(i int, j int) int {
         return j
     }
 }
+
+func nthUglyNumber(n int) int {
+    if n == 1 {
+        return 1
+    }
+    var num2, num3, num5 int
+    dp := make([]int , n)
+    dp[0] = 1
+    for i := 1; i < n; i++ {
+        dp[i] = min(dp[num2]*2, dp[num3]*3, dp[num5]*5)
+        if dp[i] == dp[num2]*2 {
+            num2++
+        }
+        if dp[i] == dp[num3]*3 {
+            num3++
+        }
+        if dp[i] == dp[num5]*5 {
+            num5++
+        }
+     }
+    return dp[n-1]
+}
+
+func min(a int, b int, c int) int {
+    if a < b {
+        if a < c {
+            return a
+        }else {
+            return c
+        }
+    }else {
+        if b < c {
+            return b
+        }else {
+            return c
+        }
+    }
+}
