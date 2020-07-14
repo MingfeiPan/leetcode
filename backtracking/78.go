3c791272f80af91576f4f0bf2d0d7a6c4015f546
Mingfei <113104667@umail.ucc.ie>
2020-07-14 23:21:05 +0800

    Subsets

--- backtracking/78.go
+++ backtracking/78.go
@@ -13,3 +13,18 @@ func bt(ret *[][]int, nums []int, cur []int, start int) {
         bt(ret, nums, append(cur, nums[i]), i+1)
     }
 }
+
+func subsets(nums []int) [][]int {
+    if len(nums) == 0 {
+        return [][]int{}
+    }
+    ret := make([][]int, 0)
+    bfs(nums, []int{}, 0, &ret)
+    return ret
+}
+func bfs(nums []int, cur []int, index int, ret *[][]int) {
+    *ret = append(*ret, append([]int{}, cur...))
+    for i := index; i < len(nums); i++ {
+        bfs(nums, append(cur, nums[i]), i+1, ret)
+    }
+}
