596eebf67aadb8eedd8f7560a398b0a044b77b17
Mingfei <113104667@umail.ucc.ie>
2020-07-21 23:01:23 +0800

    Remove Linked List Elements

--- linkedlist/203.go
+++ linkedlist/203.go
@@ -18,3 +18,24 @@ func removeElements(head *ListNode, val int) *ListNode {
     }
     return head.Next
 }
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+    dummy := &ListNode{-1, nil}
+    dummy.Next = head
+    head = dummy
+    for head != nil && head.Next != nil {
+        if head.Next.Val == val {
+            head.Next = head.Next.Next
+        }else {
+            head = head.Next    
+        }
+    }
+    return dummy.Next
+}
