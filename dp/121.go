6318b0a14ed17b02d5af55ff3744303af5526eec
Mingfei <113104667@umail.ucc.ie>
2020-08-05 23:44:42 +0800

    Best Time to Buy and Sell Stock

--- dp/121.go
+++ dp/121.go
@@ -15,3 +15,30 @@ func maxProfit(prices []int) int {
     }
     return ret
 }
+
+
+func maxProfit(prices []int) int {
+    if len(prices) == 0 {
+        return 0
+    }
+    dp := make([][]int, len(prices))
+    for i, _ := range dp {
+        dp[i] = make([]int, 2)
+    }
+    // 0表示未持有 1表示持有
+    dp[0][0] = 0
+    dp[0][1] = -prices[0]
+    for i := 1; i < len(prices); i++ {
+        dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+        dp[i][1] = max(dp[i-1][1], -prices[i])
+    }
+    return dp[len(prices)-1][0]
+}
+
+func max(a int, b int) int {
+    if a < b {
+        return b
+    }else {
+        return a
+    }
+}
