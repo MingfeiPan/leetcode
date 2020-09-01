a3332061c852218fed2a15bb37199dca47e789e6
Mingfei <113104667@umail.ucc.ie>
2020-09-01 23:38:07 +0800

    House Robber

--- dp/198.cc
+++ dp/198.cc
@@ -22,3 +22,29 @@ public:
         return dp[nums.size()];
     }
 };
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        if (nums.size() == 2) {
+            return max(nums[0], nums[1]);
+        }
+        int dp_i_1 = 0;
+        int dp_i_2 = 0;
+        int dp_i = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            dp_i = max(dp_i_1+nums[i], dp_i_2);
+            dp_i_1 = dp_i_2;
+            dp_i_2 = dp_i;
+            
+        }
+        return dp_i;
+    }
+};
