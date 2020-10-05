925cc6dbf6d2c12deafb1c5da2f60326e4324a6c
Mingfei <113104667@umail.ucc.ie>
2020-10-05 23:06:07 +0800

    Remove Covered Intervals

---
+++ sort/1288.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end(), [](std::vector<int>& a, std::vector<int>& b){
+            return a[0] < b[0];
+        });
+        int ret = 0, left = -1, right = -1;
+        for (auto& val : intervals) {
+            if (val[0] > left && val[1] > right) {
+                ret++;
+                left = val[0];
+            }
+            right = max(val[1], right);
+        }
+        return ret;
+    }
+};
