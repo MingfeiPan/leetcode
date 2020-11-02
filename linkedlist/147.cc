b43b6bbfa58be003bb336526218f12ee9111d084
Mingfei <113104667@umail.ucc.ie>
2020-11-02 23:56:17 +0800

    Insertion Sort List

--- linkedlist/147.cc
+++ linkedlist/147.cc
@@ -30,3 +30,38 @@ public:
         return dummy->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head)
+            return head;
+        ListNode *dummy = new ListNode{};
+        ListNode *pre, *cur, *temp;
+        while (head) {
+            pre = dummy;
+            cur = dummy->next;
+            while (cur) {
+                if (head->val < cur->val)
+                    break;
+                pre = pre->next;
+                cur = cur->next;
+            }
+            temp = head->next;
+            pre->next = head;
+            head->next = cur;
+            head = temp;
+        }
+        return dummy->next;
+    }
+};
