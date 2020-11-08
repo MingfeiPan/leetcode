40ac3aa9db1e9644a21ea033efdc1f01c9c6e01c
Mingfei <113104667@umail.ucc.ie>
2020-11-08 23:24:22 +0800

    Get Maximum in Generated Array

---
+++ array/1646.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        std::vector<int> nums;
+        int cur = 0, ret = 1, index = 2;
+        nums.emplace_back(0);
+        nums.emplace_back(1);
+        while (index <= n) {
+            if ((index % 2) == 0) {
+                cur = nums[index / 2];
+            } else {
+                cur = nums[(index-1)/2] + nums[(index-1)/2+1];
+            }
+            if (cur > ret) {
+                ret = cur;
+            }
+            nums.emplace_back(cur);
+            index++;
+        }
+        return ret;
+    }
+};
