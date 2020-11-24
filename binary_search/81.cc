5a64f0c8d3dc568c876e7fe2a1a6fb47d1425c47
Mingfei <113104667@umail.ucc.ie>
2020-11-24 23:38:03 +0800

    Search in Rotated Sorted Array II

---
+++ binary_search/81.cc
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int pivot = findMid(nums);
+        std::cout << pivot << std::endl;
+        int low = 0, high = nums.size() - 1, mid = 0, realmid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            realmid = (mid + pivot) % nums.size();
+            if (nums[realmid] == target) {
+                return true;
+            } else if (nums[realmid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+private:
+    int findMid(std::vector<int>& nums) {
+        int low = 0, high = nums.size() - 1, mid = 0;
+        while (low < high) {
+            while (low < high && nums[low] == nums[high])
+                ++low;    
+            mid = (low + high) / 2;
+            if (nums[mid] <= nums[high]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
