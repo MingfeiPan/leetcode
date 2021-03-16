9bcfba982f3bc78945ffa4c83d0383ff9c227485
Mingfei <113104667@umail.ucc.ie>
2021-03-16 23:07:53 +0800

    up

--- dp/121.cc
+++ dp/121.cc
@@ -17,3 +17,24 @@ public:
         return ret;
     }
 };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty())
+            return 0;
+        int hold0 = 0;
+        int nothold0 = 0;
+        int hold1 = -prices[0];
+        int nothold1 = 0;
+        for (size_t i = 1; i < prices.size(); ++i) {
+            nothold0 = std::max(hold1+prices[i], nothold1);
+            // hold0 = std::max(nothold1-prices[i], hold1); 这种 case 是可以买多次的
+            hold0 = std::max(-prices[i], hold1);
+            hold1 = hold0;
+            nothold1 = nothold0;
+        }
+        return nothold0;
+    }
+};
