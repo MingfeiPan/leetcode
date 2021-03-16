24472fcb47a6c935b3ab3d1507689659ece2ffd2
Mingfei <113104667@umail.ucc.ie>
2021-03-16 23:08:31 +0800

    Best Time to Buy and Sell Stock with Transaction Fee

--- dp/714.cc
+++ dp/714.cc
@@ -15,3 +15,22 @@ public:
         return nothold;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        if (prices.empty())
+            return 0;
+        int hold0 = 0;
+        int nothold0 = 0;
+        int hold1 = -prices[0];
+        int nothold1 = 0;
+        for (size_t i = 1; i < prices.size(); ++i) {
+            nothold0 = std::max(hold1+prices[i]-fee, nothold1);
+            hold0 = std::max(nothold1-prices[i], hold1);
+            hold1 = hold0;
+            nothold1 = nothold0;
+        }
+        return nothold0;        
+    }
+};
