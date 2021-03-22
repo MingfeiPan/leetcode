710c6e716f839ca2851030d75bcb83e31a60e0f6
Mingfei <113104667@umail.ucc.ie>
2021-03-22 22:55:29 +0800

    up

--- linkedlist/1721.cc
+++ linkedlist/1721.cc
@@ -30,3 +30,38 @@ public:
         return head;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *first = head, *pre = head;
+        while (k > 0) {
+            --k;
+            pre = pre->next;
+            if (k == 0)
+                break;
+            first = first->next;
+        }
+        ListNode* second = head;
+        while (pre) {
+            pre = pre->next;
+            second = second->next;
+        }
+        int temp = first->val;
+        first->val = second->val;
+        second->val = temp;
+        return head;
+    }
+};
+
