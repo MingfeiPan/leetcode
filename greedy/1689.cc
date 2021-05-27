b503bd6d3b1cd89a6af156c87be5db1967fba544
Mingfei <113104667@umail.ucc.ie>
2021-05-27 23:41:34 +0800

    Partitioning Into Minimum Number Of Deci-Binary Numbers

--- greedy/1689.cc
+++ greedy/1689.cc
@@ -12,3 +12,16 @@ public:
         return int(cur-'0');
     }
 };
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        int ret = 0;
+        for (const auto &c : n) {
+            ret = std::max(ret, c-'0');
+            if (ret == 9)
+                return ret;
+        }
+        return ret;
+    }
+};
