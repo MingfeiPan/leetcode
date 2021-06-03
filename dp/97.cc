002ffe181e7004c9f61125a1dc91c1b2b05a86ea
Mingfei <113104667@umail.ucc.ie>
2021-06-03 23:57:22 +0800

    Interleaving String

---
+++ dp/97.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int l1 = s1.size(), l2 = s2.size(), l3 = s3.size();
+        if ((l1+l2) != l3)
+            return false;
+        std::vector<std::vector<bool>> dp(l1+1, std::vector<bool>(l2+1, false));
+        for (int i = 0; i <= l1; ++i) {
+            for (int j = 0; j <= l2; ++j) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } else if (i == 0 && j != 0) {
+                    dp[i][j] = dp[i][j-1] && (s2[j-1] == s3[j-1]);
+                } else if (i != 0 && j == 0) {
+                    dp[i][j] = dp[i-1][j] && (s1[i-1] == s3[i-1]);
+                } else {
+                    dp[i][j] = dp[i-1][j] && (s1[i-1] == s3[i+j-1]) || dp[i][j-1] && (s2[j-1] == s3[i+j-1]);
+                }
+            }
+        }
+        return dp[l1][l2];
+    }
+};
