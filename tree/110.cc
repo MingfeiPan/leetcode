20d69205a35773df1aed0e9059c8a14b1b55ed18
Mingfei <113104667@umail.ucc.ie>
2021-06-29 23:15:57 +0800

    Balanced Binary Tree

--- tree/110.cc
+++ tree/110.cc
@@ -66,3 +66,32 @@ private:
         return std::max(left, right) + 1;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (!root)
+            return true;
+        int left = height(root->left);
+        int right = height(root->right);
+        return isBalanced(root->left) && isBalanced(root->right) && std::abs(left - right) <= 1;
+    }
+private:
+    int height(TreeNode* root) {
+        if (!root)
+            return 0;
+        return std::max(height(root->left), height(root->right)) + 1;
+    }
+};
