02ba3ad2d6456563a454a37bb1004af9bc2557fa
Mingfei <113104667@umail.ucc.ie>
2021-07-11 23:51:30 +0800

    Kth Smallest Element in a Sorted Matrix

--- heap/378.cc
+++ heap/378.cc
@@ -20,3 +20,23 @@ public:
         return q.top();
     }
 };
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        auto cmp = [](int a, int b) {
+            return a > b;       
+        };
+        std::priority_queue<int, std::vector<int>, decltype(cmp)> q(cmp);
+        for (const auto& line : matrix) {
+            for (const auto& num : line) {
+                q.emplace(num);
+            } 
+        }
+        while (k > 1) {
+            q.pop();
+            --k;
+        }
+        return q.top();
+    }
+};
