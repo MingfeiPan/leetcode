0ced5544319ddd6660733df3a528623469a45ac8
Mingfei <113104667@umail.ucc.ie>
2021-07-11 23:36:54 +0800

    Minimum Depth of Binary Tree

--- tree/111.cc
+++ tree/111.cc
@@ -30,3 +30,36 @@ public:
             dfs(root->right, ret, cur+1);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        int ret = INT_MAX;
+        dfs(root, ret, 0);
+        return ret;
+    }
+    void dfs(TreeNode* root, int& ret, int cur) {
+        if (!root->left && !root->right) {
+            if ((cur+1) < ret) {
+                ret = cur + 1;
+            }
+        }
+        if (root->left)
+            dfs(root->left, ret, cur+1);
+        if (root->right)
+            dfs(root->right, ret, cur+1);
+    }
+};
