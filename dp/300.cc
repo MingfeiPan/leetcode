ea64ace839f0b487c71de62b2999f1dc92d52325
Mingfei <113104667@umail.ucc.ie>
2021-07-11 23:55:52 +0800

    Longest Increasing Subsequence

--- dp/300.cc
+++ dp/300.cc
@@ -18,3 +18,20 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        std::vector<int> dp(nums.size(), 1);
+        int ret = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = std::max(dp[i], dp[j]+1);
+                }
+            }
+            ret = std::max(dp[i], ret);
+        }
+        return ret;
+    }
+};
