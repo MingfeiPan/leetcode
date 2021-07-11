e867c7844317c6a4fa320f51afdca7a898a15b3b
Mingfei <113104667@umail.ucc.ie>
2021-07-11 23:46:34 +0800

    Populating Next Right Pointers in Each Node

--- tree/116.cc
+++ tree/116.cc
@@ -56,3 +56,61 @@ public:
         return root;
     }
 };
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+struct Item {
+    int level;
+    Node* root;
+};    
+public:
+    Node* connect(Node* root) {
+        if (!root)
+            return root;
+        std::queue<Item*> q;
+        Item *i = new Item{0, root};
+        q.emplace(i);
+        Node* pre = nullptr;
+        int index = 0;
+        while (!q.empty()) {
+            Item *cur = q.front();
+            q.pop();
+            if (cur->level == index) {
+                if (pre) {
+                    pre->next = cur->root;
+                    pre = pre->next;
+                }
+            } else {
+                pre = cur->root;
+                ++index;
+            }
+            if (cur->root->left) {
+                Item *temp = new Item{cur->level+1, cur->root->left};
+                q.emplace(temp);
+            }
+            if (cur->root->right) {
+                Item *temp = new Item{cur->level+1, cur->root->right};
+                q.emplace(temp);
+            }
+        }
+        return root;        
+    }
+};
