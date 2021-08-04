44f6223bb88f578600ac5786baf3e7dd45a04e3a
Mingfei <113104667@umail.ucc.ie>
2021-08-04 23:22:36 +0800

    Sum of Left Leaves

--- tree/404.cc
+++ tree/404.cc
@@ -32,3 +32,36 @@ private:
         dfs(root->right, 1, sum);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root)
+            return 0;
+        int ret = 0;
+        recur(root, ret, 0);
+        return ret;
+    }
+private:
+    void recur(TreeNode* root, int& ret, int flag) {
+        if (!root->left && !root->right && flag == 1) {
+            ret += root->val;
+            return;
+        }
+        if (root->left)
+            recur(root->left, ret, 1);
+        if (root->right)
+            recur(root->right, ret, 0);
+    }
+};
