a45b7ebb012306070d81028efbd9005ff2749a92
Mingfei <113104667@umail.ucc.ie>
2021-08-04 23:25:21 +0800

    Path Sum II

--- tree/113.cc
+++ tree/113.cc
@@ -44,3 +44,44 @@ private:
         }        
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        std::vector<std::vector<int>> ret;
+        if (!root)
+            return ret;
+        std::vector<int> cur;
+        int sum = 0;
+        recur(root, ret, cur, sum, targetSum);
+        return ret;
+    }
+private:
+    void recur(TreeNode* root, std::vector<std::vector<int>>& ret, std::vector<int> cur, int sum, int targetSum) {
+        if (!root->left && !root->right && (sum+root->val) == targetSum) {
+            cur.emplace_back(root->val);
+            ret.emplace_back(cur);
+            return;
+        }
+        cur.emplace_back(root->val);
+        sum += root->val;
+        if (root->left) {
+            recur(root->left, ret, cur, sum, targetSum);
+        }
+        if(root->right) {
+            recur(root->right, ret, cur, sum, targetSum);
+        }
+        return;
+    }
+};
