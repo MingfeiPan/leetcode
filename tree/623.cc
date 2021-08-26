c076bee263ce6e931bed610808631651e83ca373
Mingfei <113104667@umail.ucc.ie>
2021-08-26 23:50:44 +0800

    Add One Row to Tree

--- tree/623.cc
+++ tree/623.cc
@@ -54,3 +54,56 @@ public:
         return root;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if (depth == 1) {
+            TreeNode* newroot = new TreeNode{val};
+            newroot->left = root;
+            return newroot;
+        }
+        recur(root, 2, val, depth);
+        return root;
+    }
+private:
+    void recur(TreeNode* root, int level, int& val, int& depth) {
+        if (!root)
+            return;
+        if (level == depth) {
+            TreeNode* newleft = new TreeNode{val};
+            TreeNode* newright = new TreeNode{val};
+            if (root->left) {
+                auto left = root->left;
+                root->left = newleft;
+                newleft->left = left;
+            } else {
+                root->left = newleft;
+            }
+            if (root->right) {
+                auto right = root->right;
+                root->right = newright;
+                newright->right = right;
+            } else {
+                root->right = newright;
+            }
+            return;
+        }
+        if (level < depth) {
+            recur(root->left, level+1, val, depth);
+            recur(root->right, level+1, val, depth);
+        }
+        return;
+    }
+};
