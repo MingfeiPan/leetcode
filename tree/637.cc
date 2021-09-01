dbaed543002b3fbe27602435085561f0c817fb81
Mingfei <113104667@umail.ucc.ie>
2021-09-01 23:11:03 +0800

    up

--- tree/637.cc
+++ tree/637.cc
@@ -50,3 +50,49 @@ public:
         
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+struct Item {
+    int level;
+    TreeNode* root;
+};
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        std::vector<double> ret;    
+        std::queue<Item> q;
+        q.emplace(Item{0, root});
+        int index = 0, count = 0;
+        double total = 0.0;
+        while (!q.empty()) {
+            Item i = q.front();
+            q.pop();
+            if (i.level != index) {
+                ret.emplace_back(total / count);                  
+                ++index;
+                total = 0.0;
+                count = 0;
+            }
+            total += i.root->val;
+            ++count;
+            if (i.root->left)
+                q.emplace(Item{i.level+1, i.root->left});
+            if (i.root->right)
+                q.emplace(Item{i.level+1, i.root->right});
+        }
+        if (count != 0) {
+            ret.emplace_back(total / count);
+        }
+        return ret;
+    }
+};
