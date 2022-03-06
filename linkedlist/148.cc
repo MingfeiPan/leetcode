9041c10e25e7c404de5bbb9d0d488e7f2258ee5b
Mingfei <113104667@umail.ucc.ie>
2022-03-07 00:11:22 +0800

    Sort List

--- linkedlist/148.cc
+++ linkedlist/148.cc
@@ -30,3 +30,56 @@ public:
         return dummy->next;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* mid = findMid(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return merge(left, right);
+    }
+private:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* head = dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                head->next = l1;
+                l1 = l1->next;
+            } else {
+                head->next = l2;
+                l2 = l2->next;
+            }
+            head = head->next;
+        }
+        if (l1)
+            head->next = l1;
+        if (l2)
+            head->next = l2;
+        return dummy->next;
+    }
+    ListNode* findMid(ListNode* head) {
+        ListNode *slow = head, *temp;
+        while (head && head->next) {
+            temp = slow;
+            slow = slow->next;
+            head = head->next->next;
+        }
+        if (slow == head) return slow;
+        temp->next = nullptr;
+        return slow;
+    }
+};
