f9d4616f42fb40f81ca94ee3421299476cea5483
Mingfei <113104667@umail.ucc.ie>
2022-03-20 22:43:12 +0800

    Rotate List

--- linkedlist/61.cc
+++ linkedlist/61.cc
@@ -40,3 +40,38 @@ public:
         return dummy;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head) return nullptr;
+        ListNode* tail = head;
+        int count = 1;
+        while (tail->next) {
+            tail = tail->next;
+            ++count;
+        }
+        if (k % count == 0) return head;
+        k = k % count;
+        k = count - k;
+        ListNode* pre = head;
+        while (k > 1) {
+            pre = pre->next;
+            --k;
+        }
+        ListNode* new_head = pre->next;
+        tail->next = head;
+        pre->next = nullptr;
+        return new_head;
+    }
+};
